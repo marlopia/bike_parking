9b797de645c2759066d2a84ea5139d44c93d1707
Mario Lopez Gonzalez <marlopiacj@gmail.com>
2025-11-25 15:33:38 +0100

    Docstrings y tests para es_email_valido

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -35,3 +35,46 @@ def test_dni_valido_invalido_formato(test_input):
 def test_dni_valido_sin_letra(test_input):
     """Comprueba que el validador de DNI rechace DNIs sin letra"""
     assert es_dni_valido(test_input) == False
+
+
+# es_email_valido
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        "ana@example.com",
+        "test@gmail.es",
+        "okay@yahoo.fr",
+        "valid@outlook.it",
+        "correct@proton.tv",
+    ],
+)
+def test_es_email_valido_correcto(test_input):
+    """Comprueba que el validador de emails devuelva True para emails válidos"""
+    assert es_email_valido(test_input) == True
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        "anaexample.com",
+        "testgmail.es",
+        "okayyahoo.fr",
+        "validoutlook.it",
+        "correctproton.tv",
+    ],
+)
+def test_email_valido_sin_arroba(test_input):
+    """Comprueba que el validador de emails devuelva False para emails que no tengan un @ (arroba)"""
+    assert es_email_valido(test_input) == False
+
+
+@pytest.mark.parametrize("test_input", ["ana@", "test@", "okay@", "valid@", "correct@"])
+def test_email_valido_sin_dominio(test_input):
+    """Comprueba que el validador de emails devuelva False para emails que no tengan un dominio"""
+    assert es_email_valido(test_input) == False
+
+
+@pytest.mark.parametrize("test_input", [""])
+def test_email_valido_vacio(test_input):
+    """Comprueba que el validador de emails devuelva False si el string es vacío"""
+    assert es_email_valido(test_input) == False
