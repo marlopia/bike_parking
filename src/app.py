70c0cbc5d782c0f42508436f722a6af6b28d5647
Mario Lopez Gonzalez <marlopiacj@gmail.com>
2025-11-27 18:15:05 +0100

    Pytest cov 97%

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
 """Archivo principal de la aplicación, controla el ciclo de vida"""
 
-from parking.data_utils.csv_utils import asegurar_csvs
-from parking.config import OPCIONES, TITULO
-from parking.ui_utils.ui_console import clear_screen, manage_options, pause, render_menu
+# los comentarios de pragma son para que pytest-cov no cuente el modulo
+from parking.data_utils.csv_utils import asegurar_csvs  # pragma: no cover
+from parking.config import OPCIONES, TITULO  # pragma: no cover
+from parking.ui_utils.ui_console import (
+    clear_screen,
+    manage_options,
+    pause,
+    render_menu,
+)  # pragma: no cover
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     asegurar_csvs()
 
     eleccion = render_menu(TITULO, OPCIONES)
